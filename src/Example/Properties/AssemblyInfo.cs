9092e6d61c828427c074490b3fdb3b38f6751ed4
George Pollard <porges@porg.es>
2013-04-10 18:23:48 +1200

    might as well stamp it

--- src/Example/Properties/AssemblyInfo.cs
+++ src/Example/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Example")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © George Pollard 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
